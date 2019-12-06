05e585d25991515ac568920f3f643f7d26baf865
Anton Satorin <brayan.1702@mail.ru>
2019-12-06 23:35:41 +0400

    06.12.19

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,11 @@
+<!-- INITIALIZATION AND ADD CURRENT SLIDE NUMBER -->
+const glide = new Glide('.glide').mount();
+const slide = document.getElementsByClassName('slide-number')[0];
+
+glide.on('run.after', event =>{
+    slide.innerText = glide.index + 1;
+});
+
 const header = document.getElementsByTagName('header')[0];
 const logo = document.getElementsByClassName('logo-img')[0];
 const logoImg = logo.getElementsByTagName('img')[0];
@@ -5,11 +13,12 @@ const number = document.getElementsByClassName('number')[0];
 const menuButton = document.getElementsByClassName('menu-button')[0];
 const menuButtonImg = menuButton.getElementsByTagName('img')[0];
 const menu = document.getElementsByTagName('menu')[0];
-const hideButtons = [...document.getElementsByClassName('hide-button')];
+const openInfoButtons = [...document.getElementsByClassName('open-info-button')];
 const carouselLists = [...document.getElementsByClassName('carousel-list')];
 const carouselTitles = [...document.getElementsByClassName('carousel-title')];
 const carouselDescription = [...document.getElementsByClassName('carousel-description')];
 
+<!-- CHANGE MENU WHEN SCROLLING -->
 document.addEventListener('scroll', event =>{
     if(pageYOffset !== 0){
         changeMenuToBlackTheme()
@@ -19,6 +28,7 @@ document.addEventListener('scroll', event =>{
     }
 });
 
+<!-- CHANGE MENU WHEN CLICK MENU BUTTON -->
 menuButton.addEventListener('click', event =>{
    if(menu.style.display === 'flex'){
        menu.style.display = 'none';
@@ -51,24 +61,11 @@ menuButton.addEventListener('click', event =>{
    }
 });
 
-
-hideButtons.forEach(element =>{
+openInfoButtons.forEach(element =>{
     element.addEventListener('click', event =>{
-        if(carouselLists[0].style.display !== 'none'){
-            hideButtons.forEach(element =>{
-                element.innerText = 'раскрыть';
-            });
-            carouselTitles.forEach(element =>{
-                element.style.display = 'none';
-            });
-            carouselDescription.forEach(element=>{
-               element.style.display = 'none'
-            });
-            carouselLists.forEach(element =>{
-                element.style.display = 'none';
-            })
-        }else{
-            hideButtons.forEach(element =>{
+        console.log(carouselLists[0]);
+        if(carouselLists[0].style.display !== 'block'){
+            openInfoButtons.forEach(element =>{
                 element.innerText = 'скрыть';
             });
             carouselTitles.forEach(element =>{
@@ -80,9 +77,23 @@ hideButtons.forEach(element =>{
             carouselLists.forEach(element =>{
                 element.style.display = 'block';
             })
+        }else{
+            openInfoButtons.forEach(element =>{
+                element.innerText = 'раскрыть';
+            });
+            carouselTitles.forEach(element =>{
+                element.style.display = 'none';
+            });
+            carouselDescription.forEach(element=>{
+                element.style.display = 'none'
+            });
+            carouselLists.forEach(element =>{
+                element.style.display = 'none';
+            })
         }
     });
 });
+
 
 if(pageYOffset !== 0){
     changeMenuToBlackTheme();
